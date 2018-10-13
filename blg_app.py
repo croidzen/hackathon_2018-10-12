329ce1faf3a117f7ae8024ed1516319d74b92368
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2018-10-13 15:59:57 +0200

    added delting database

--- blg_app.py
+++ blg_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, abort
-import csv
+import csv, datetime, os
 
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ def hello_world():
 
 
 def write_entry_to_file(entry):
-    with open('database.csv', mode='a', newline='') as file:
+    with open('database.csv', mode='a+', newline='') as file:
         file_writer = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         # file_writer.writerow(['Success!'])
         file_writer.writerow([entry['timestamp'], entry['vehicle_id']]) #, entry['changed_state'], entry['new_state']])
@@ -21,16 +21,28 @@ def write_entry_to_file(entry):
 def create_entry():
     # if not request.json in request.json:    #or not 'timestamp' or...
     #     abort(400)
+    
+    dt = datetime.datetime.now()
+    ts = str(dt.year) + '-' + str(dt.month)+ '-' + str(dt.day)+ '_' + str(dt.hour)+ ':' + str(dt.minute)+ ':' + str(dt.second)
+
     entry = {
-        'timestamp': 'todo',
+        'timestamp': ts,
         'vehicle_id': request.json['vehicle_id']
         # 'changed_state': request.json['changed_state'],
         # 'new_state': request.json['new_state']
     }
     write_entry_to_file(entry)
     return jsonify({'entry': entry}), 201
-# curl -i -H "Content-Type: application/json" -X POST -d "{"""vehicle_id""": """001"""}" http://localhost:5000/
+# curl -i -H "Content-Type: application/json" -X POST -d "{"""vehicle_id""": """12345"""}" http://localhost:5000/
+# curl -i -H "Content-Type: application/json" -X POST -d "{"""vehicle_id""": """12345"""}" https://blg-challenge.herokuapp.com/
 
+
+@app.route('/', methods=['DELETE'])
+def delete_database():
+    if os.path.exists('database.csv'):
+        os.remove('database.csv')
+    return jsonify({'result': True})
+# curl -i -H "Content-Type: application/json" -X DELETE -d http://localhost:5000/
 
 if __name__ == '__main__':
     app.run(debug=True) 
