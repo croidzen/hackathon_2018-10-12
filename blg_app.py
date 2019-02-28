2a0b8ad5290309606b2ec2deb5476f3a06d84698
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2019-02-28 21:18:22 +0100

    cleanup

--- blg_app.py
+++ blg_app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify, abort
-import csv, datetime, os, json
-
+from flask import Flask, request, jsonify
+import csv
+import datetime
+import os
+import json
 
 app = Flask(__name__)
 
@@ -37,11 +39,12 @@ def index():
     return 'BLG-Challenge Hackathon Bremen 2.0'
 
 
-
 def write_entry_to_file(entry):
     with open('database.csv', mode='a+', newline='') as file:
-        file_writer = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        file_writer.writerow([entry['timestamp'], entry['vehicle_id'], entry['changed_state'], entry['new_state']])
+        file_writer = csv.writer(file, delimiter='\t', quotechar='"',
+                                    quoting=csv.QUOTE_MINIMAL)
+        file_writer.writerow([entry['timestamp'], entry['vehicle_id'],
+                                entry['changed_state'], entry['new_state']])
 
 
 @app.route('/api', methods=['POST'])
@@ -49,7 +52,8 @@ def create_entry():
     # if not request.json in request.json:    #or not 'timestamp' or...
     #     abort(400)
     dt = datetime.datetime.now()
-    ts = str(dt.year) + '-' + str(dt.month)+ '-' + str(dt.day)+ '_' + str(dt.hour)+ ':' + str(dt.minute)+ ':' + str(dt.second)
+    ts = str(dt.year) + '-' + str(dt.month) + '-' + str(dt.day) + '_' + str(
+        dt.hour) + ':' + str(dt.minute) + ':' + str(dt.second)
 
     entry = {
         'timestamp': ts,
@@ -59,8 +63,10 @@ def create_entry():
     }
     write_entry_to_file(entry)
     return jsonify({'entry': entry}), 201
-    # curl -i -H "Content-Type: application/json" -X POST -d "{"""vehicle_id""": """12345"""}" http://localhost:5000/api
-    # curl -i -H "Content-Type: application/json" -X POST -d "{"""vehicle_id""": """12345"""}" https://blg-challenge.herokuapp.com/api
+    # curl -i -H "Content-Type: application/json" -X POST -d "{
+    # """vehicle_id""": """12345"""}" http://localhost:5000/api
+    # curl -i -H "Content-Type: application/json" -X POST -d "{
+    # """vehicle_id""": """12345"""}" https://blg-challenge.herokuapp.com/api
 
 
 @app.route('/api', methods=['GET'])
@@ -79,8 +85,11 @@ def delete_database():
     if os.path.exists('database.csv'):
         os.remove('database.csv')
     return jsonify({'result': True})
-    # curl -i -H "Content-Type: application/json" -X DELETE -d "" http://localhost:5000/api
-    # curl -i -H "Content-Type: application/json" -X DELETE -d "" https://blg-challenge.herokuapp.com/api  
+    # curl -i -H "Content-Type: application/json" -X DELETE -d "
+    # " http://localhost:5000/api
+    # curl -i -H "Content-Type: application/json" -X DELETE -d "
+    # " https://blg-challenge.herokuapp.com/api
+
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
